46383c1b763aa3e0028a4a88efbe2b0f6f9b166c
Naveensai1998 <149163524+Naveensai1998@users.noreply.github.com>
2024-07-24 13:57:20 +0530

    example.py

--- exmple.py
+++ exmple.py
@@ -1,6 +1,15 @@
-for i in range(1,10):
-    if i%2==0:
-        i+=1
-        print(i)
-    else:
-        print('not prime')
+pipeline {
+    agent {
+        label 'ITE_BEA2_EB'
+    }
+    stages {
+        stage('Get Agent IP Address') {
+            steps {
+                script {
+                    def agentIP = bat(script: 'powershell.exe -Command "(Test-Connection -ComputerName localhost -Count 1).IPV4Address.IPAddressToString"', returnStdout: true).trim()
+                    echo "IP address of agent: ${agentIP}"
+                }
+            }
+        }
+    }
+}
